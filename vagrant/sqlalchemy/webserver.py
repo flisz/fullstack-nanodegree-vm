30e7dd687f6daa101ce22427f555fdb3a8cafeac
Francis Laszlo Imre Sziebert <franksziebert@gmail.com>
2019-03-07 19:32:54 -0700

    added webserverHandler a BaseHTTPRequestHandler

--- vagrant/sqlalchemy/webserver.py
+++ vagrant/sqlalchemy/webserver.py
@@ -1,5 +1,23 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
+class webserverHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		try:
+			if self.path.endswith("/hello"):
+				self.send_response(200)
+				self.send_header('Content-type','text/html')
+				self.end_headers()
+
+				output = ''
+				output += "<html><body>Hello!</body></html>"
+				self.wfile.write(output)
+				print output
+				return 
+		except IOError:
+			self.send_error(404, "File Not Found: {}".format(self.path))
+
+
+
 def main():
 	try:
 		port = 8080
@@ -11,6 +29,6 @@ def main():
 		print("^C entered, stopping web server...")
 		server.socket.close()
 
-		
+
 if __name__ == '__main__':
 	main()
