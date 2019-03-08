b40354f1a1ca7714ad4dc258ec1eec242388b4a4
Francis Laszlo Imre Sziebert <franksziebert@gmail.com>
2019-03-07 19:28:31 -0700

    lotsofmenus and webserver demo beginning

---
+++ vagrant/sqlalchemy/webserver.py
@@ -0,0 +1,16 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+def main():
+	try:
+		port = 8080
+		server = HTTPServer(('',port), webserverHandler)
+		print("Web server running on port {}".format(port))
+		server.serve_forever()
+
+	except KeyboardInterrupt:
+		print("^C entered, stopping web server...")
+		server.socket.close()
+
+		
+if __name__ == '__main__':
+	main()
