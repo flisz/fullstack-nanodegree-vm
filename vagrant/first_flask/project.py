58604a33f97fe5b4e475f1e30f97a3e0d58a33fb
Francis Laszlo Imre Sziebert <franksziebert@gmail.com>
2019-04-07 23:26:20 -0600

    fixed project

--- vagrant/first_flask/project.py
+++ vagrant/first_flask/project.py
@@ -18,49 +18,57 @@ session = DBSessionMaker()
 def restaurants():
     restaurants = session.query(Restaurant)
     restaurants_headers = restaurants.column_descriptions
-    output = ''
     # Main Table:
-    table_object = Restaurant
-    subtable_objects = MenuItem
-    output += html_table_with_subtables(output, 
-                                        table_object=table_object, 
-                                        subtable_objects=subtable_objects)
+    output = html_restaurants()
     return output
 
-
-def html_table_with_subtables(output, table_object = None, subtable_objects = None, next_function = None):
-    addition += html_table(output, table_object=table_object, subtable_objects=subtable_objects, next_function=html_subtables)
-    # todo: log additions
-    output += addition
-    return output
-
-
-def html_subtables(output, subtable_objects = None, next_function = None):
-    if subtable_objects:
-        for subtable in subtable_objects:
-            output += html_table(output, table_object = subtable, next_function = None)
-    # todo: log messages
-    return output
-
-
-def html_table(output, table_object = None, next_function = None):
-    items = session.query(table_object)
-    item_headers = items.column_descriptions
-    for header in item_headers:
-        output += "<th>{}</th>".format(header)
-    for item in items:
+def html_restaurants():
+    restaurants = session.query(Restaurant)
+    restaurant_headers = Restaurant.__table__.columns.keys()
+    output = ''
+    for restaurant in restaurants:
         # todo: generalize 
-        for column in items.__table__.columns.keys():
-            column_data = getattr(item, header)
+        
+        print("restaurant_headers: {}".format(restaurant_headers))
+        output += "<table>"
+        output += "<tr>"
+        for header in restaurant_headers:
+            output += "<th>{}</th>".format(header)
+        output += "</tr>"
+        output += "<tr>"
+        for header in restaurant_headers:
+            column_data = getattr(restaurant, header)
             output += "<td>{}</td>".format(column_data)
+        output += "</tr>"
         output += "</table>"
         output += "<br>"
-        if next_function:
-            # todo: log messages
-            return next_function(output, args=*args, kwargs=**kwargs)
+        addition = html_menu_items(restaurant)
+        output += addition
+                
     # todo: log messages
     return output
 
+def html_menu_items(restaurant):
+    menu_items = session.query(MenuItem).filter(MenuItem.restaurant == restaurant)
+    item_headers = MenuItem.__table__.columns.keys()
+    output = "<table>"
+    output += "<tr>"
+    for header in item_headers:
+        output += "<th>{}</th>".format(header)
+    output += "</tr>"
+    for item in menu_items:
+        # todo: generalize 
+        item_headers = MenuItem.__table__.columns.keys()
+        print("item_headers: {}".format(item_headers))
+        output += "<tr>"
+        for header in item_headers:
+            column_data = getattr(item, header)
+            output += "<td>{}</td>".format(column_data)
+        output += "</tr>"
+    output += "</table>"
+    output += "<br>"
+    return output
+        
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
