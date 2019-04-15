0ddeb1a2dfca064ee3b6382aed5b6682133379a2
Francis Laszlo Imre Sziebert <franksziebert@gmail.com>
2019-04-14 19:35:49 -0600

    adding html templates

--- vagrant/catalog/config.py
+++ vagrant/catalog/config.py
@@ -2,19 +2,16 @@ ENGINE = 'postgres'
 USER = 'vagrant'
 PASSWORD = None
 if PASSWORD is None:
-	USER_PASSWORD = USER
-else: 
-	USER_PASSWORD = USER + ':' + PASSWORD
-
+    USER_PASSWORD = USER
+else:
+    USER_PASSWORD = USER + ':' + PASSWORD
 HOST = 'localhost'
 PORT = 5432
-
 if PORT is None:
-	HOST_PORT = HOST
+    HOST_PORT = HOST
 else:
-	HOST_PORT = HOST + ':' + str(PORT)
-
-DB_NAME = 'restaurant'
+    HOST_PORT = HOST + ':' + str(PORT)
+DB_NAME = 'catalog'
 POSTGRES_LOGIN = DB_NAME
-SQL_COMMAND = '{}:///{}'.format(ENGINE,POSTGRES_LOGIN)
+SQL_COMMAND = '{}:///{}'.format(ENGINE, POSTGRES_LOGIN)
 print('Using SQL_COMMAND: {}'.format(SQL_COMMAND))
--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -12,19 +12,19 @@ Base = declarative_base()
 
 
 class Category(Base):
-	__tablename__ = 'restaurant'
-	name = Column(String(80), nullable=False)
-	id = Column(Integer, primary_key=True)
+    __tablename__ = 'restaurant'
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
 
 
 class Item(Base):
-	__tablename__ = 'menu_item'
-	name = Column(String(80),nullable=False)
-	id = Column(Integer, primary_key=True)
-	description = Column(String(250))
-	category_id = Column( Integer, ForeignKey('category_id.id'))
-	category = relationship(Category)
+    __tablename__ = 'menu_item'
+    name = Column(String(80),nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    category_id = Column( Integer, ForeignKey('category_id.id'))
+    category = relationship(Category)
+
 
 engine = create_engine(SQL_COMMAND)
-
 Base.metadata.create_all(engine)
