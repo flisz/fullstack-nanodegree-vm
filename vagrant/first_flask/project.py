72b8fa4e16687acafca7fa764a0bd6762173d343
Francis Laszlo Imre Sziebert <franksziebert@gmail.com>
2019-04-29 15:27:56 -0600

    menu cleanup

--- vagrant/first_flask/project.py
+++ vagrant/first_flask/project.py
@@ -63,7 +63,7 @@ def html_table_restaurant(output = None, verified = None,
             column_data = getattr(restaurant, header)
             output += "<td>{}</td>".format(column_data)
         menu_path = "/restaurant/{}/menu".format(restaurant.id)
-        output += '<td><a href="{}">Menu</a></td>'.format(edit_path)
+        output += '<td><a href="{}">Menu</a></td>'.format(menu_path)
         if verified is True:
             edit_path = "/restaurant/{}/edit".format(restaurant.id)
             delete_path = "/restaurant/{}/delete".format(restaurant.id)
@@ -105,47 +105,34 @@ def site_add_restaurant(verified):
 @app.route('/restaurant/<int:restaurant_id>/menu')
 @app.route('/restaurant/<int:restaurant_id>/')
 def restaurant_menu(restaurant_id):
+    print(restaurant_id)
     restaurants = session.query(Restaurant).filter(Restaurant.id==restaurant_id)
+    menu_items = session.query(MenuItem).filter(MenuItem.restaurant_id==restaurant_id)
     verified = True
-    restaurant_headers = None
     menu_item_headers = None
-    output = site_restaurants_with_menu_items(restaurants = restaurants, restaurant_headers = restaurant_headers, menu_item_headers = menu_item_headers, verified = verified)
+    output = site_restaurants_with_menu_items(restaurants = restaurants, 
+                                              menu_items = menu_items, menu_item_headers = menu_item_headers, 
+                                              verified = verified)
     return output
 
 
 def site_restaurants_with_menu_items(output=None, verified = None,
-                                     restaurants = None, restaurant_headers = None, restaurant_id = None,  
-                                     menu_items = None, menu_item_headers = None, menu_item_id = None):
-    if verified is True:
-        if output is None: 
-            output = ''
-        if menu_item_id is None:
-            if restaurant_id is not None:
-                restaurants = session.query(Restaurant).filter(Restaurant.id == restaurant_id)
-            if restaurants is None:
-                restaurants = session.query(Restaurant)
-            for restaurant in restaurants:
-                # todo: generalize 
-                add_restaurant = html_table_restaurant(restaurant, restaurant_headers = restaurant_headers)
-                menu_items = session.query(MenuItem).filter(MenuItem.restaurant == restaurant)
-                add_menu_items = html_table_menu_items(restaurants = restaurant, menu_item_headers = menu_item_headers)
-                output += add_restaurant + add_menu_items        
-        else:  # if menu_item_id is not None: 
-            menu_items = session.query(MenuItem).filter(MenuItem.id == menu_item_id)
-            if restaurant_id is None or menu_item.restaurant_id == restaurant_id:
-                restaurants = session.query(Restaurant).filter(Restaurant.id == menu_item.restaurant_id)
-            else:  # restaurant_id is not None
-                raise ValueError("resturant_id:{} and menu_item.resturant_id:{} do not match")
-            for restaurant in restaurants:
-                output += "<h2>{}: (#{})<h2>".format(restaurant.name, restaurant.id)
-                output += "<h2>Menu:<h2>"
-                add_menu_items = html_table_menu_items(restaurants = restaurant, menu_items = menu_items, menu_item_headers = menu_item_headers)
-                output += add_restaurant + ad_menu_items         
+                                     restaurants = None,  
+                                     menu_items = None, menu_item_headers = None):
+        for restaurant in restaurants:
+            output += "<h2>{}: (#{})<h2>".format(restaurant.name, restaurant.id)
+            if verified is True: 
+                '<h4><a href="/restaurant/{}/menu/add">Add Menu Item</a></h4>'.format(restaurant.id)
+            output += "<h3>Menu:<h3>"
+            add_menu_item_table = html_table_menu_items(verified = verified,
+                          restaurant = restaurant, 
+                          menu_items = menu_items, menu_item_headers = menu_item_headers)
+            output += add_menu_item_table        
         return output
 
 
 def html_table_menu_items(verified = None,
-                          restaurants = None, 
+                          restaurant = None, 
                           menu_items = None, menu_item_headers = None):
     if restaurants is None:
         restaurants = session.query(Restaurant)
@@ -175,6 +162,8 @@ def html_table_menu_items(verified = None,
                 output += '<th>Edit</th>'
                 output += '<th>Delete</th>'
         if verified is True:
+            edit_path = "/restaurant/{}/{}/edit".format(restaurant.id,menu_item.id)
+            delete_path = "/restaurant/{}/{}/delete".format(restaurant.id,menu_item.id)
             output += '<td><a href="{}"}>Edit</a></td>'.format(edit_path)
             output += '<td><a href="{}"}>Delete</a></td>'.format(delete_path)
         output += "</tr>"
